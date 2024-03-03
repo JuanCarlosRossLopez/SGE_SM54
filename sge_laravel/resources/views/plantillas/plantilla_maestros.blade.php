35da67fe3571b043f412524dbc4660486fbba0e4
Wilberth Antony Cahuich Cruz <22393239@utcancun.edu.mx>
2024-03-02 22:18:25 -0500

    implementacion del sidebar de maestros y adaptacion del diseño

---
+++ sge_laravel/resources/views/plantillas/plantilla_maestros.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('titulo')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    @vite('resources/css/app.css')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <script src="/js/app.js"></script>
+</head>
+
+<body class="w-screen flex flex-col">
+    <div class="bg-[#2F4050] w-fit min-h-screen fixed flex items-start">
+        <button class="text-gray-300 hover:text-white transition-all text-2xl w-fit" onclick="mostrarMenu()">
+            <i class="fa-solid fa-bars p-2"></i>
+        </button>
+    </div>
+    <div class="w-screen flex flex-row fixed" id="miDiv">
+        <div class=" bg-[#2F4050] w-[20rem] h-screen flex flex-col justify-between">
+            <div>
+                <a href="/" class="text-white flex flex-row align-items-center p-3 pb-3 gap-2 hover:scale-105 transition-all">
+                    <span class="text-4xl font-bold">SGE</span>
+                    <span class="border-l-2 pl-2">Sistema de Gestión de Estadías.</span>
+                </a>
+                <ul class="flex flex-col justify-between">
+
+                    <div>
+                        <!--Linea de separación atte: guayabo -->
+                        <p class="border-top opacity-60"></p>
+                        <li>
+                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
+                                <i class="fa-solid fa-school"></i>
+                                Inicio
+                            </button>
+                        </li>
+                        <li>
+                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
+                                <i class="fa-solid fa-users-line"></i>
+                                Asesorados
+                            </button>
+                        </li>
+                        <li>
+                            <button class="buttons_sidebar text-start text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
+                                <i class="fa-solid fa-diagram-project"></i>
+                                Gestión de proyectos
+                                de asesorados
+                            </button>
+                        </li>
+                        <li>
+                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
+                                <i class="fa-solid fa-file"></i> Generacion de informes </button>
+                        </li>
+                        <li>
+                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
+                                <i class="fa-solid fa-book"></i>Historial de memorias
+                        </li>
+                        <li>
+                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
+                                <i class="fa-solid fa-envelope"></i>
+                                Notificaciones
+                            </button>
+                        </li>
+                        <li>
+                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
+                                <i class="fa-solid fa-address-card"></i>
+                                Mi perfil
+                            </button>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+            <div>
+                <!--Linea de separación atte: guayabo -->
+                <p class="border-top opacity-60"></p>
+
+                <button class="logout_sidebar justify-start align-items-center text-[#c8c8c8] hover:text-white flex flex-row gap-2 w-full hover:bg-[#4D6781]  mb-2 hover:scale-110 hover:w-[96%]">
+                    <i class="fa-solid fa-right-from-bracket "></i>
+                    Cerrar sesión
+                </button>
+            </div>
+
+        </div>
+        <div class="w-screen min-h-screen bg-black bg-opacity-50 flex items-center pl-2">
+            <button class="text-gray-300 text-4xl hover:text-white hover:scale-125 transition-all" onclick="ocultarMenu()">
+                <i class="fa-solid fa-angle-left"></i>
+            </button>
+        </div>
+    </div>
+
+    <main class="w-full pl-8">
+        <div class="flex justify-left">
+            <div class="w-full m-4">
+                @yield('contenido')
+            </div>
+        </div>
+    </main>
+</body>
+
+<!--Icons - realmente estos fueron que mas me convencieron atte: guayabo -->
+<script src="https://kit.fontawesome.com/61439499b0.js" crossorigin="anonymous"></script>
+
+</html>
--- sge_laravel/resources/views/test.blade.php
+++ sge_laravel/resources/views/test.blade.php
@@ -1,54 +1,9 @@
-@extends('welcome')
-
+@extends('plantillas.plantilla_maestros')
 @section('titulo')
-    Ferrecun nunca muere
+Maestros dashboard
 @endsection
-
 @section('contenido')
-    <div class="flex justify-left">
-        <div class="w-full m-4">
-            <h2 class="sombra_letras text-2xl text-[#000000] font-bold mb-4 uppercase">Inicio - test</h2>
-
-            <div class="flex justify-between mb-4">
-                <button class="bg-green-700 h-fit text-white p-2 rounded-md flex flex-row items-center gap-2 shadow-md">
-                    <h3 class="text-lg font-bold">Crear usuarios</h3>
-                    <i class="fa-solid fa-user-plus"></i>
-                </button>
-                <div class="flex flex-row items-center h-fit bg-white border p-2 rounded-lg shadow-md">
-                    <input class="w-[25rem] h-6 text-base font-normal pl-2 border-white" type="text"
-                        placeholder="Buscar por matricula/nomina" />
-                    <button class="w-fit">
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                    </button>
-                </div>
-            </div>
-
-            <div class="overflow-x-auto shadow-md">
-                <table class="table-auto w-full border-collapse border border-gray-300">
-                    <thead>
-                        <tr class="bg-[#434343] text-[#b9b9b9] uppercase">
-                            <th class="text-center w-fit">#</th>
-                            <th class="text-center w-fit">Nombre usuario</th>
-                            <th class="text-center w-fit">Nomina/Matrícula</th>
-                            <th class="text-center w-fit">Email</th>
-                            <th class="text-center w-fit">Rol asignado</th>
-                            <th class="text-center w-fit">Opciones</th>
-                        </tr>
-                    </thead>
-                    <tbody class=" transition-colors bg-white">
-                        <tr class="hover:bg-[#cacaca]">
-                            <td class="border text-center">1</td>
-                            <td class="border text-center">Marco H.</td>
-                            <td class="border text-center">22786654</td>
-                            <td class="border text-center">marco.hau@test.com</td>
-                            <td class="border text-center">docente</td>
-                            <td class="border py-2 text-center flex flex-row items-center gap-2 justify-center">
-                                editar ver eliminar
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+<div class=" min-h-screen flex justify-center items-center"  >
+    Contenido
+</div>
 @endsection
