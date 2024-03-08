3a218d64645d43a4bd3e62b09358f276f063faf2
Jacob0418 <cjosecoronado97@gmail.com>
2024-03-08 09:50:22 -0500

    cambio formulario

--- sge_laravel/resources/views/students/activities_calendar.blade.php
+++ sge_laravel/resources/views/students/activities_calendar.blade.php
@@ -5,7 +5,8 @@
 @section('contenido')
     <h1 class="text-4xl font-normal uppercase mb-5">Calendario de Actividades</h1>
     <div class="flex flex-wrap justify-center items-center gap-4 md:gap-6 lg:gap-8">
-        <div class="border-2 border-[#18A689] rounded-[7px_7px_7px_7px] p-3 bg-[#F6F6F6] shadow w-full max-w-[220px] md:max-w-[715px] lg:max-w-[645px] h-[410px]">
+        <div
+            class="border-2 border-[#18A689] rounded-[7px_7px_7px_7px] p-3 bg-[#F6F6F6] shadow w-full max-w-[220px] md:max-w-[715px] lg:max-w-[645px] h-[410px]">
             <h1 class="font-semibold text-xl text-[#18A689] md:text-3xl">Recordatorios</h1>
             <div class="overflow-auto h-[325px] p-2">
                 <div class="flex flex-col">
@@ -37,7 +38,8 @@
                 </div>
             </div>
         </div>
-        <div class="border shadow bg-[#ceeae4] md:p-3 p-0 justify-center flex flex-col h-[410px] w-[220px] md:w-[715px] lg:w-[645px] rounded-[7px_7px_7px_7px]">
+        <div
+            class="border shadow bg-[#ceeae4] md:p-3 p-0 justify-center flex flex-col h-[410px] w-[220px] md:w-[715px] lg:w-[645px] rounded-[7px_7px_7px_7px]">
             <p class="text-[#18A689] font-semibold text-3xl text-left ml-9 mb-4">Febrero</p>
             <table>
                 <thead>
@@ -63,11 +65,13 @@
                     </tr>
                     <tr>
                         <td class="text-center p-3">4</td>
-                        <td class="text-center p-3 transition-transform hover:scale-110 duration-100 cursor-pointer"><span class="border-[#2F4050] p-2 rounded-[7px_7px_7px_7px] bg-[#2F4050] text-white">5</span></td>
+                        <td class="text-center p-3 transition-transform hover:scale-110 duration-100 cursor-pointer"><span
+                                class="border-[#2F4050] p-2 rounded-[7px_7px_7px_7px] bg-[#2F4050] text-white">5</span></td>
                         <td class="text-center p-3">6</td>
                         <td class="text-center p-3">7</td>
                         <td class="text-center p-3">8</td>
-                        <td class="text-center p-3 transition-transform hover:scale-110 duration-100 cursor-pointer"><span class="border-[#07D7A9] p-2 rounded-[7px_7px_7px_7px] bg-[#07D7A9] text-white">9</span></td>
+                        <td class="text-center p-3 transition-transform hover:scale-110 duration-100 cursor-pointer"><span
+                                class="border-[#07D7A9] p-2 rounded-[7px_7px_7px_7px] bg-[#07D7A9] text-white">9</span></td>
                         <td class="text-center p-3">10</td>
                     </tr>
                     <tr>
@@ -82,7 +86,11 @@
                     <tr>
                         <td class="text-center p-3">18</td>
                         <td class="text-center p-3">19</td>
-                        <td class="text-center p-3 transition-transform hover:scale-110 duration-100 cursor-pointer"><span class="border-[#07D7A9] p-2 rounded-[7px_7px_7px_7px] bg-[#07D7A9] text-white">20</span></td>
+                        <td class="text-center p-3 transition-transform hover:scale-110 duration-100 cursor-pointer"><span
+                                class="border-[#07D7A9] p-2 rounded-[7px_7px_7px_7px] bg-[#07D7A9] text-white">
+                                20
+                            </span>
+                        </td>
                         <td class="text-center p-3">21</td>
                         <td class="text-center p-3">22</td>
                         <td class="text-center p-3">23</td>
--- sge_laravel/resources/views/students/anteproyecto.blade.php
+++ sge_laravel/resources/views/students/anteproyecto.blade.php
@@ -4,79 +4,72 @@
     SGE
 @endsection
 
-@section('formulario')
+@section('contenido')
     <h1 class=" text-4xl font-normal uppercase mb-4">Gestión de Anteproyecto</h1>
-    <div class="w-[90%] bg-[#F6F6F6] p-6 rounded-md border-[#18A689] border-1 md:ml-16">
+    <form class="shadow-md w-11/12 bg-[#F6F6F6] p-3 rounded mx-auto">
+        <h1 class="text-lg text-center font-medium uppercase my-4">Datos Anteproyecto</h1>
+        <div class="flex flex-col mb-3 items-center">
+            <label for="titulo" class="text-lg font-medium">Título del proyecto</label>
+            <p class="italic mt-2">El título debe ser claro y consciso</p>
+            <input type="text" id="titulo" name="titulo"
+                class="outline-none focus:border-[#18A689] block shadow-sm w-1/2 sm:text-sm border-gray-300 rounded-md">
+            {{-- <i
+                class="fa-regular fa-comment fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal"></i>
+            <i
+                class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i> --}}
+        </div>
+        <div class="flex flex-row -mx-2 w-full">
+            <div class="flex flex-col w-1/2 px-2 gap-4">
 
-        <form class="space-y-6">
-            <div>
-                <label for="titulo" class="block text-sm font-medium">Título del proyecto</label>
-                <div class="flex items-center">
-                    <input type="text" id="titulo" name="titulo"
-                        class="mt-1 outline-none focus:border-[#18A689] block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                    <i
+                <div class="flex flex-col">
+                    <label for="titulo" class="text-lg font-medium">Objetivo General</label>
+                    <p class="italic mt-2">El objetivo debe describir lo que se busca hacer con el proyecto</p>
+                    <textarea type="text" id="titulo" name="titulo"
+                        class="outline-none focus:border-[#18A689] block shadow-sm sm:text-sm border-gray-300 rounded-md h-28"></textarea>
+                    {{-- <i
                         class="fa-regular fa-comment fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal"></i>
                     <i
-                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i>
+                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i> --}}
                 </div>
-            </div>
-            <div>
-                <label for="objetivo_general" class="block text-sm font-medium">Objetivo General</label>
-                <div class="flex items-center">
-                    <textarea id="objetivo_general" name="objetivo_general" rows="4"
-                        class="mt-1 outline-none focus:border-[#18A689] block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
-                    <i
+                <div class="flex flex-col">
+                    <label for="titulo" class="text-lg font-medium">Objetivo Específicos</label>
+                    <p class="italic mt-2">Los objetivos específicos deben ser objetivos del general</p>
+                    <textarea type="text" id="titulo" name="titulo"
+                        class="outline-none focus:border-[#18A689] block shadow-sm sm:text-sm border-gray-300 rounded-md h-28"></textarea>
+                    {{-- <i
                         class="fa-regular fa-comment fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal"></i>
                     <i
-                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i>
+                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i> --}}
                 </div>
             </div>
-
-            <div>
-                <label for="objetivos_especificos" class="block text-sm font-medium">Objetivos Específicos</label>
-                <div class="flex items-center">
-                    <textarea id="objetivos_especificos" name="objetivos_especificos" rows="4"
-                        class="mt-1 outline-none focus:border-[#18A689] block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
-                    <i
+            <div class="flex flex-col w-1/2 px-2 gap-4">
+                <div class="flex flex-col">
+                    <label for="titulo" class="text-lg font-medium">Alcance del Proyecto</label>
+                    <p class="italic mt-2">Describir el objetivo del proyecto</p>
+                    <textarea type="text" id="titulo" name="titulo"
+                        class="outline-none focus:border-[#18A689] block shadow-sm sm:text-sm border-gray-300 rounded-md h-28"></textarea>
+                    {{-- <i
                         class="fa-regular fa-comment fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal"></i>
                     <i
-                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i>
+                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i> --}}
                 </div>
-            </div>
-
-            <div>
-                <label for="alcance_proyecto" class="block text-sm font-medium">Alcance del proyecto</label>
-                <div class="flex items-center">
-                    <textarea id="alcance_proyecto" name="alcance_proyecto" rows="4"
-                        class="mt-1 outline-none focus:border-[#18A689] block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
-                    <i
+                <div class="flex flex-col">
+                    <label for="titulo" class="text-lg font-medium">Justificación</label>
+                    <p class="italic mt-2">Describir la justificación del proyecto</p>
+                    <textarea type="text" id="titulo" name="titulo"
+                        class="outline-none focus:border-[#18A689] block shadow-sm sm:text-sm border-gray-300 rounded-md h-28"></textarea>
+                    {{-- <i
                         class="fa-regular fa-comment fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal"></i>
                     <i
-                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i>
+                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i> --}}
                 </div>
             </div>
+        </div>
+        <div class="flex flex-col mt-3">
+            <button class="border p-3 rounded bg-[#18A689] text-white w-24 hover:bg-[#306157] mx-auto">Guardar</button>
+        </div>
+    </form>
 
-            <div>
-                <label for="justificacion" class="block text-sm font-medium">Justificación</label>
-                <div class="flex items-center">
-                    <textarea id="justificacion" name="justificacion" rows="4"
-                        class="mt-1 outline-none focus:border-[#18A689] block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
-                    <i
-                        class="fa-regular fa-comment fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal"></i>
-                    <i
-                        class="fa-regular fa-pen-to-square fa-lg ml-2 text-[#18A689] transition-transform hover:scale-110 duration-75 cursor-pointer show-modal-edit"></i>
-                </div>
-            </div>
-
-            <div class="flex justify-center max-w-6xl items-center mx-auto">
-                <button type="submit"
-                    class="p-2 h-12 w-24 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-[#2F4050] hover:bg-[#18A689] outline-none">
-                    Enviar
-                </button>
-            </div>
-
-        </form>
-    </div>
     <div class="modal h-screen w-full fixed left-0 top-0 hidden flex justify-center items-center bg-black bg-opacity-50 ">
         <div class="bg-[#01A080] w-[80%] rounded shadow-lg max-w-4xl md:mx-auto">
             <div class="border-b px-4 py-2 flex justify-between items-center">
@@ -106,7 +99,8 @@
             </div>
         </div>
     </div>
-    <div class="modal-edit h-screen w-full fixed left-0 top-0 hidden flex justify-center items-center bg-black bg-opacity-50 ">
+    <div
+        class="modal-edit h-screen w-full fixed left-0 top-0 hidden flex justify-center items-center bg-black bg-opacity-50 ">
         <div class="bg-[#01A080] w-[80%] rounded shadow-lg max-w-4xl md:mx-auto">
             <div class="border-b px-4 py-2 flex justify-between items-center">
                 <h3 class="font-semibold text-lg text-white mx-auto">Resolución de comentario</h3>
@@ -118,8 +112,8 @@
             <div class="bg-white p-2">
                 <div class="modal-body mb-0 h-[23vh]">
                     <label for="titulo" class="block text-sm font-medium">Título del proyecto</label>
-                        <input type="text" id="titulo" name="titulo"
-                            class="mt-1 outline-none focus:border-[#18A689] block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    <input type="text" id="titulo" name="titulo"
+                        class="mt-1 outline-none focus:border-[#18A689] block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                     <div class="flex justify-center items-center w-full border-t pt-2">
                         <button class="bg-blue-600 hover:bg-blue-700 p-2 py-1 rounded text-white mt-3">Resolver</button>
                     </div>
@@ -132,20 +126,20 @@
             const modal = document.querySelector(modalSelector);
             const showTriggers = document.querySelectorAll(triggerSelector);
             const closeModalButtons = modal.querySelectorAll('.close-modal');
-    
+
             showTriggers.forEach(trigger => {
                 trigger.addEventListener('click', function() {
                     modal.classList.remove('hidden');
                 });
             });
-    
+
             closeModalButtons.forEach(button => {
                 button.addEventListener('click', function() {
                     modal.classList.add('hidden');
                 });
             });
         }
-    
+
         initializeModalEvents('.show-modal', '.modal');
         initializeModalEvents('.show-modal-edit', '.modal-edit');
     </script>
