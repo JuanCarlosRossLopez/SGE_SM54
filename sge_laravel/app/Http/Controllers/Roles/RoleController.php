55efb055acc38c2682cf497a63339421180076bd
Wilberth Antony Cahuich Cruz <22393239@utcancun.edu.mx>
2024-03-10 12:56:25 -0500

    paquete para usar roles

--- sge_laravel/app/Http/Controllers/Roles/RoleController.php
+++ sge_laravel/app/Http/Controllers/Roles/RoleController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\Rols;
+namespace App\Http\Controllers\Roles;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class RolControllers extends Controller
+class RoleController extends Controller
 {
     /**
      * Display a listing of the resource.
