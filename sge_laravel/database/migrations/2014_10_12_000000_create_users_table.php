5e9fde830c066b106a3354fc3e48663999795110
Wilberth Antony Cahuich Cruz <22393239@utcancun.edu.mx>
2024-03-10 12:58:06 -0500

    eliminacion de rol

--- sge_laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ sge_laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('rol_id')->constrained('rols')
-                ->onUpdate('restrict')
-                ->onDelete('restrict');
             $table->rememberToken();
             $table->timestamps();
         });
