f89499a95b161926515a84d6171faeeb6d5faddc
Jonathan0890 <jonathan.coox15@gmail.com>
2024-03-11 12:41:36 -0500

    es un pequeño avanze

--- sge_laravel/resources/views/registro.blade.php
+++ sge_laravel/resources/views/registro.blade.php
@@ -4,39 +4,41 @@
 @endsection
 @section('contenido')
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <button class="show-modal">
-        modal
-    </button>
+    <button class="show-modal bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600">
+        Registro Masivo</button>
     <div class="modal-wrapper">
-        <div class="modal  fixed top-0 left-0 w-full h-[700px] overflow-hidden flex justify-center items-center bg-opacity-500">
+        <div
+            class="modal fixed top-0 left-0 w-full h-[700px] overflow-hidden flex justify-center items-center bg-opacity-500">
             <div class="bg-green-500 w-[600px] rounded shadow-lg max-w-4xl ">
                 <div class="border-b px-4 py-2 flex justify-between items-center">
-                    <h3 class="font-semibold text-lg justify-center items-center text-white text-center">Registrar Alumnos</h3>
+                    <h3 class="font-semibold text-lg justify-center items-center text-white text-center">
+                    </h3>
                     <button class="close-modal bg-white rounded-full">
                         <p class="text-2xl"><i class="fa-solid fa-circle-xmark" style="color: #d50101;"></i></p>
                     </button>
                 </div>
                 <div class="bg-white p-2">
-                    <div class="modal-body mb-0 overflow-y-auto h-[100vh]">
+                    <div class="modal-body mb-0 overflow-y-auto h-[50vh]"> <!-- se mueve el lugar del espacio-->
                         <div class="container mx-auto px-4 py-8">
                             <h1 class="text-center font-semibold text-2xl">Agregar Alumnos</h1>
-                            <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                            <div class="grid gap-8"> <!-- El centro del registro -->
                                 <div class="bg-white rounded-lg p-8 mx-auto">
-                                    <form action="#funciones futura" method="POST" class="w-full">
+                                    <form action="{{ route('students.store') }} " method="POST" class="w-full"><!-- funcion futura del ingresar -->
+                                        <!-- Cambiar la funcion por la nueva funcion -->
                                         @csrf
                                         <div class="mb-4">
-                                            <label for="matricula" class="text-gray-700 block mb-2">MATRÍCULA:</label>
-                                            <textarea name="matricula" id="matricula" class="w-full md:w-96 border rounded-lg py-2 px-4"></textarea>
+                                            <label for="id_student" class="text-gray-700 block mb-2">MATRÍCULAS:</label>
+                                            <textarea name="id_student" id="id_student" class="w-full md:w-96 border rounded-lg py-2 px-4"></textarea>
                                         </div>
                                         <div class="mb-4">
-                                            <label for="grupo" class="text-gray-700 block mb-2">GRUPO:</label>
-                                            <input type="text" name="grupo" id="grupo"
+                                            <label for="group" class="text-gray-700 block mb-2">GRUPO:</label>
+                                            <input type="text" name="group" id="group"
                                                 class="form-control w-full border rounded-lg py-2 px-4">
                                         </div>
                                         <div class="text-center">
                                             <button type="submit"
                                                 class="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600">Agregar
-                                                Usuario</button>
+                                                Usuarios</button>
                                         </div>
                                     </form>
                                 </div>
@@ -46,9 +48,8 @@
                 </div>
             </div>
         </div>
-
-
     </div>
+
 
     <script>
         const tableBody = document.querySelector('tbody');
