274e61ed00ea5e8c87be615f454a6d183e469c63
cristian770h <cristianlopezrodriguez770@gmail.com>
2024-03-11 23:34:47 -0500

    Manana se chambea

--- sge_laravel/resources/views/super_admin/dashboard/dashboard.blade.php
+++ sge_laravel/resources/views/super_admin/dashboard/dashboard.blade.php
@@ -18,14 +18,54 @@ Panel de control administrador
     </div>
     <!-- cuerpo del dashboard -->
     <div>
-        <div>
-            <h1>Grafica anteproyecots</h1>
+        <!-- cuerpo del dashboard -->
+    <div class="flex flex-row px-12 space-x-11">
+        <div class="bg-white w-[800px] h-96 rounded-md flex items-cente">
+            
+                <img src="{{ asset('image/Group 16.png') }}" alt="Logo Image" class=" object-cover m-auto mt-10 ">
+            
         </div>
-        <div>
-            <h1>
-                Proyectos
-            </h1>
+        <div class="bg-white w-[450px] h-96 rounded-md ">
+            <img src="{{ asset('image/Group 9.png') }}" alt="Logo Image" class=" object-cover m-auto mt-10 ">
         </div>
+    </div>
+    <div class="flex flex-row py-12 px-12 space-x-3  ">
+        <div class="bg-white  w-80 h-80 rounded-md text-center">
+            <h1 class="poppins-light m-3">Historial de memoria</h1>
+            <img src="{{ asset('image/file-circle-info_1.png') }}" alt="Logo Image" class="object-cover m-auto mt-10">
+            <button class="bg-[#18A689] text-white w-32 h-9 rounded-md text-sm">Visualizar</button>
+        </div>
+        <div class="bg-white  w-80 h-80 rounded-md text-center">
+            <h1 class="poppins-light m-3">Proyectos finalizados</h1>
+        <img src="{{ asset('image/features.png') }}" alt="Logo Image" class="object-cover m-auto mt-10 ">
+        <button class="bg-[#18A689] text-white w-32 h-9 rounded-md text-sm bottom-6">Visualizar</button>
+        </div>
+        <div class="bg-white w-80 h-80 rounded-md text-center">
+        <h1 class="poppins-light m-3">Proyectos aprobados</h1>
+        <img src="{{ asset('image/memo-circle-check 1.png') }}" alt="Logo Image" class="object-cover m-auto mt-10">
+        <button class="bg-[#18A689] text-white w-32 h-9 rounded-md text-sm">Visualizar</button>
+
+        </div>
+        <div class="bg-white  w-80 h-80 rounded-md text-center">
+        <h1 class="poppins-light m-3"> Asesores</h1>
+        <img src="{{ asset('image/workshop 1.png') }}" alt="Logo Image" class="object-cover m-auto mt-10">
+        <button class="bg-[#18A689] text-white w-32 h-9 rounded-md text-sm">Visualizar</button>
+        </div>
+
+        
+    </div>
+    <div class="py-12 px-12">
+    <div class="bg-white w-auto h-80  flex justify-center">
+        <div>
+            <img src="{{ asset('image/Group 16.png') }}" alt="Logo Image" class="object-cover m-auto mt-10">
+        </div>
+    </div>
+    </d>
+  
+   
+</div>
+
+        
     </div>
 </div>
 @endsection
