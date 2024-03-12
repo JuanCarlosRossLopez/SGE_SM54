fcc2261db1043882a31940593d3adeef1efa2388
JuanCarlosRossLopez <berpyboy10@gmail.com>
2024-03-11 23:24:25 -0500

    Correción de migración de usuarios

--- sge_laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ sge_laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,10 +19,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-<<<<<<< HEAD
-=======
-            $table->rememberToken();
->>>>>>> 8374ac6e760520fb6129b556c3b8402abfa9020a
             $table->timestamps();
         });
     }
