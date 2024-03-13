5ddf47f2c28edc175f3801e5d0ab701d4dc3800a
JuanCarlosRossLopez <berpyboy10@gmail.com>
2024-03-13 09:15:13 -0500

    merge con r rama Ross

--- sge_laravel/resources/views/UserManagement/users.blade.php
+++ sge_laravel/resources/views/UserManagement/users.blade.php
@@ -3,24 +3,13 @@
 @section('titulo', 'Usuarios')
 @section('contenido')
 
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    </head>
-    <div class="view_conteiner">
-        <div class="back_conteiner">
-            <div class="top_conteiner">
-                <label>
-                    Gestión de Usuarios
-                </label>
-                <label>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-8 h-8">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
-                    </svg>
-                </label>
-            </div>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+</head>
+<div class="view_conteiner">
+
 
             <div class="content_conteiner">
                 <label class="word_title">Tabla de Gestión de Alumnos</label>
@@ -136,15 +125,15 @@
 
 
 
-                <div
-                    class="modal3 h-screen w-full fixed left-0 top-0 hidden flex justify-center items-center bg-black bg-opacity-50 ">
-                    <div class="bg-[#01A080] w-full rounded shadow-lg max-w-sm">
-                        <div class="border-b px-4 py-2 flex justify-between items-center">
-                            <h3 class="font-semibold text-lg text-white text-center flex-grow">Agregar Usuario</h3>
-                            <button class="close-modal3 bg-white rounded-full h-[1rem] flex items-center">
-                                <p class="text-2xl"><i class="fa-solid fa-circle-xmark" style="color: #d50101;"></i></p>
-                            </button>
-                        </div>
+        <div
+            class="modal3 h-screen w-full fixed left-0 top-0 hidden flex justify-center items-center bg-black bg-opacity-50 ">
+            <div class="bg-[#01A080] w-full rounded-t-lg shadow-lg max-w-sm">
+                <div class="border-b px-4 py-2 flex justify-between items-center">
+                    <h3 class="font-semibold text-lg text-white text-center flex-grow">Agregar Usuario</h3>
+                    <button class="close-modal3 bg-white rounded-full h-[1rem] flex items-center">
+                        <p class="text-2xl"><i class="fa-solid fa-circle-xmark" style="color: #d50101;"></i></p>
+                    </button>
+                </div>
 
 
                         <div class="modal-container">
@@ -173,10 +162,10 @@
                                         <input type="text" name="username" class="rounded input-field">
                                     </div>
 
-                                    <div class="mb-4">
-                                        <label class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                                        <input type="email" name="email" class="rounded input-field">
-                                    </div>
+                            <div class="mb-4">
+                                <label class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                                <input type="email" name="email" class="rounded input-field">
+                            </div>
 
                                     <div class="mb-4">
                                         <label class="block text-gray-700 text-sm font-bold mb-2">Contraseña</label>
@@ -194,20 +183,20 @@
 
                                         </select>
 
-                                    <div class="flex justify-center">
-                                        <button type="submit">
-                                            <div
-                                                class=" bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">
-                                                Agregar usuario
-                                            </div>
-                                        </button>
-
+                            <div class="flex justify-center">
+                                <button type="submit">
+                                    <div
+                                        class=" bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">
+                                        Agregar usuario
                                     </div>
-                                </form>
+                                </button>
+
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
+            </div>
+        </div>
 
 
 
@@ -275,19 +264,19 @@
                 <script>
                     const modal3 = document.querySelector('.modal3');
 
-                    const showModal3 = document.querySelector('.show-modal3');
-                    const closeModal3 = document.querySelectorAll('.close-modal3');
+            const showModal3 = document.querySelector('.show-modal3');
+            const closeModal3 = document.querySelectorAll('.close-modal3');
 
-                    showModal3.addEventListener('click', function() {
-                        modal3.classList.remove('hidden')
-                    })
+            showModal3.addEventListener('click', function() {
+                modal3.classList.remove('hidden')
+            })
 
-                    closeModal3.forEach(close => {
-                        close.addEventListener('click', function() {
-                            modal3.classList.add('hidden')
-                        });
-                    });
-                </script>
+            closeModal3.forEach(close => {
+                close.addEventListener('click', function() {
+                    modal3.classList.add('hidden')
+                });
+            });
+            </script>
 
                 <script>
                     const modal4 = document.querySelector('.modal4');
@@ -304,29 +293,29 @@
                         })
                     })
 
-                    closeModal4.forEach(close => {
-                        close.addEventListener('click', function() {
-                            modal4.classList.add('hidden')
-                        });
-                    });
-                </script>
+            closeModal4.forEach(close => {
+                close.addEventListener('click', function() {
+                    modal4.classList.add('hidden')
+                });
+            });
+            </script>
 
-                <script>
-                    const modal5 = document.querySelector('.modal5');
+            <script>
+            const modal5 = document.querySelector('.modal5');
 
-                    const showModal5 = document.querySelector('.show-modal5');
-                    const closeModal5 = document.querySelectorAll('.close-modal5');
+            const showModal5 = document.querySelector('.show-modal5');
+            const closeModal5 = document.querySelectorAll('.close-modal5');
 
                     showModal5.addEventListener('click', function() {
                         modal5.classList.remove('hidden')
                     })
                 </script>
 
-                <script>
-                    const modal6 = document.querySelector('.modal6');
+            <script>
+            const modal6 = document.querySelector('.modal6');
 
-                    const showModal6 = document.querySelector('.show-modal6');
-                    const closeModal6 = document.querySelectorAll('.close-modal6');
+            const showModal6 = document.querySelector('.show-modal6');
+            const closeModal6 = document.querySelectorAll('.close-modal6');
 
                     showModal6.addEventListener('click', function() {
                         modal6.classList.remove('hidden')
