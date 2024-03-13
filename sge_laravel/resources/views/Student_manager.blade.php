e98417706ea82cc2b6332fe8cf2629da3728b1fe
Jomaruwu03 <jomar.jaae@gmail.com>
2024-03-12 20:22:04 -0500

    cambios

---
+++ sge_laravel/resources/views/Student_manager.blade.php
@@ -0,0 +1,10 @@
+@extends('templates.template_admin')
+@section('titulo')
+    Gestor de Estudiantes
+@endsection
+@section('contenido')
+   <h1 class="">Lista de Estudiantes</h1>
+    <a href="/registro" class="btn btn-primary">Crear Estudiante</a>
+   
+@endsection
+
--- sge_laravel/resources/views/templates/template_admin.blade.php
+++ sge_laravel/resources/views/templates/template_admin.blade.php
@@ -5,43 +5,32 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('titulo')</title>
+    <title>@yield('title')</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     @vite('resources/css/app.css')
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            ocultarMenu(); // Ocultar el menú al cargar la página por primera vez
-        });
-
-        function ocultarMenu() {
-            var div = document.getElementById("miDiv");
-            div.classList.add("hidden");
-        }
-
-        function mostrarMenu() {
-            var div = document.getElementById("miDiv");
-            div.classList.remove("hidden");
-        }
-    </script>
+    <link href="{{ asset('css/font.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/progress.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" rel="stylesheet">
+  <!-- Font Awesome for icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
-<body class="w-screen flex flex-col">
+<body class="w- flex flex-col overflow-x-hidden">
 
     <div class="bg-[#2F4050] w-fit min-h-screen fixed flex items-start">
-        <button class="text-gray-300 text-2xl w-fit  hover:text-white transition-all" onclick="mostrarMenu()">
+        <button class="text-gray-300 hover:text-white transition-all text-2xl w-fit" onclick="mostrarMenu()">
             <i class="fa-solid fa-bars p-2"></i>
         </button>
     </div>
-    <div class="w-screen flex flex-row fixed" id="miDiv">
-        <div class=" bg-[#2F4050] w-[20rem] h-screen flex flex-col justify-between">
+    <div class="w-screen flex flex-row fixed" id="sidebar">
+        <div class="sidebar_background">
             <div>
-                <a href="/" class="text-white flex flex-row align-items-center p-3 pb-3 gap-2 transition-all  hover:scale-105 ">
-                    <span class="text-4xl font-bold">SGE</span>
-                    <span class="border-l-2 pl-2">Sistema de Gestión de Estadías.</span>
+                <a href="/">
+                    <img src="{{ asset('image/SGE_BLANCO_150px.svg') }}" alt="Login Image" class="">
                 </a>
                 <ul class="flex flex-col justify-between">
 
@@ -49,42 +38,77 @@
                         <!--Linea de separación atte: guayabo -->
                         <p class="border-top opacity-60"></p>
                         <li>
-                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 hover:bg-[#4D6781] hover:text-white transition-all  hover:scale-110 hover:w-[96%]">
-                                <i class="fa-solid fa-school"></i>
+                            <button class="buttons_sidebar " onclick="location.href='/dashboard_alumno'">
+                                <i class="fa-solid fa-school"></i></i>
                                 Inicio
                             </button>
                         </li>
                         <li>
-                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 hover:bg-[#4D6781] hover:text-white transition-all  hover:scale-110 hover:w-[96%]">
-                                <i class="fa-solid fa-users-line"></i>
+                            <button class="buttons_sidebar " onclick="location.href='/anteproyecto'">
+                                <i class="fa-solid fa-school"></i></i>
+                                Gestión de Anteproyectos
+                            </button>
+                        </li>
+
+                        <li>
+                            <a href="/usuarios" class="buttons_sidebar ">
+                                <i class="fa-solid fa-school"></i>
                                 Usuarios
-                            </button>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/alumnos_asesorados" class="buttons_sidebar">
+                                <i class="fa-solid fa-users-line"></i>
+                                Alumnos asesorados
+                            </a>
                         </li>
                         <li>
-                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 hover:bg-[#4D6781] hover:text-white transition-all  hover:scale-110 hover:w-[96%]">
-                                <i class="fa-solid fa-diagram-project"></i>
-                                Gestión de proyectos
-                            </button>
+                            <a href="/dashboard_asesor" class="buttons_sidebar">
+                                <i class="fa-solid fa-chalkboard-user"></i>
+                                Inicio Asesor
+                            </a>
                         </li>
                         <li>
-                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
-                                <i class="fa-solid fa-file"></i> Generacion de informes </button>
+                            <a href="/panel_admin" class="buttons_sidebar">
+                                <i class="fa-solid fa-chalkboard-user"></i>
+                                Inicio Admin
+                            </a>
                         </li>
                         <li>
-                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
-                                <i class="fa-solid fa-book"></i>Historial de memorias
+                            <a href="/Dashboard_Direccion" class="buttons_sidebar">
+                                <i class="fa-solid fa-chalkboard-user"></i>
+                                Inicio Dirección
+                            </a>
                         </li>
                         <li>
-                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 transition-all hover:bg-[#4D6781] hover:text-white hover:scale-110 hover:w-[96%]">
-                                <i class="fa-solid fa-envelope"></i>
-                                Notificaciones
-                            </button>
+                            <a href="/dashboard-presidencial" class="buttons_sidebar ">
+                                <i class="fa-solid fa-school"></i>
+                                Inicio Presidente
+                            </a>
+                        <li>
+                            <a href="/historial-memorias" class="buttons_sidebar">
+                                <i class="fa-solid fa-landmark"></i>
+                                Historial de memorias
+                            </a>
                         </li>
                         <li>
-                            <button class="buttons_sidebar text-[#c8c8c8] w-full flex flex-row align-items-center items-center gap-2 hover:bg-[#4D6781] hover:text-white transition-all  hover:scale-110 hover:w-[96%]" onclick="location.href='/Perfil_Admin'">
+                            <a href="/informes" class="buttons_sidebar ">
+                                <i class="fa-solid fa-file"></i> Generacion de informes
+                            </a>
+                        </li>
+                        <li>
+                            <a href='/Perfil_Estudiante' class="buttons_sidebar">
                                 <i class="fa-solid fa-address-card"></i>
                                 Mi perfil
-                            </button>
+                            </a>
+
+                        </li>
+                        <li>
+                            <a class="buttons_sidebar" href="/roles">
+                                <i class="fa-solid fa-cog "></i>
+                                Asignación de roles
+                            </a>
                         </li>
                     </div>
                 </ul>
@@ -92,32 +116,54 @@
             <div>
                 <!--Linea de separación atte: guayabo -->
                 <p class="border-top opacity-60"></p>
-
-                <button class="logout_sidebar justify-start align-items-center text-[#c8c8c8] hover:text-white flex flex-row gap-2 w-full hover:bg-[#4D6781] mb-2 transition-all  hover:scale-110 hover:w-[96%] ">
-                    <i class="fa-solid fa-right-from-bracket"></i>
+                <button class="logout_sidebar buttons_sidebar">
+                    <i class="fa-solid fa-right-from-bracket "></i>
                     Cerrar sesión
                 </button>
             </div>
+
         </div>
-        <div class="w-screen min-h-screen bg-black bg-opacity-50 flex items-center pl-2">
+        <div class="sidebar_active">
             <!--lo puse negro xq se ve mejor atte: jotomar -->
-            <button class="text-black text-4xl hover:text-black hover:scale-125 transition-all" onclick="ocultarMenu()">
+            <button class="button_hide_sidebar" onclick="ocultarMenu()">
                 <i class="fa-solid fa-angle-left"></i>
             </button>
         </div>
     </div>
 
-    <main class="w-full pl-8">@yield('contenido')</main>
-    <!--aberracion de front pero sirve atte: jotomar -->
+    <div class="content_main_footer">
+        <main class="main">
+            <div class="flex justify-left">
+                <div class="w-screen h-full">
+                    @yield('contenido')
+                </div>
+            </div>
+        </main>
+        <footer class="footer">
+            <div>
+                <p class="text-gray-700">Copyright Universidad Tecnológica de Cancún © 2024</p>
+            </div>
+        </footer>
+    </div>
 
-    <footer class="absolute bottom-0 left-7 inset-y-full inset-x-3/4">
-        <div class="container">
-            <p class="text-gray-700"></p>
-        </div>
-    </footer>
 </body>
 
 <!--Icons - realmente estos fueron que mas me convencieron atte: guayabo -->
 <script src="https://kit.fontawesome.com/61439499b0.js" crossorigin="anonymous"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        ocultarMenu()
+    })
+
+    function ocultarMenu() {
+        var div = document.getElementById("sidebar")
+        div.classList.add("hidden")
+    }
+
+    function mostrarMenu() {
+        var div = document.getElementById("sidebar")
+        div.classList.remove("hidden")
+    }
+</script>
 
 </html>
