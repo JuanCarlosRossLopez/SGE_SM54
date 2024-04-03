6dd3ef7f10c5e303352c03b971776f3bf2053116
NierRep24 <danielsanchezmartinez064@gmail.com>
2024-04-02 23:11:16 -0500

    botonsitos en el dash alumno

--- sge_laravel/resources/views/students/activities_calendar.blade.php
+++ sge_laravel/resources/views/students/activities_calendar.blade.php
@@ -169,7 +169,51 @@
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                    </svg></button>
+                    </svg>
+                </button>
+
+            </div>
+            <div class=" mt-4 border-[#18A689]">
+                <button
+                    class="bg-[#18A689] p-2 flex flex-row gap-2 text-white rounded transition-transform hover:scale-110">
+                    <a href="/aprobacion" target="_blank">
+                        Desacargar
+                        Carta Aprobación de Memoria
+                    </a>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                    </svg>
+                </button>
+            </div>
+            <div class=" mt-4 border-[#18A689]">
+                <button class="bg-[#18A689] p-2 flex flex-row gap-2 text-white rounded transition-transform hover:scale-110">
+                    <a href="/autodigit" target="_blank">
+                        Desacargar
+                        Autorización de Digitalización
+                    </a>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                    </svg>
+                </button>
+            </div>
+            <div class=" mt-4 border-[#18A689]">
+                <button
+                    class="bg-[#18A689] p-2 flex flex-row gap-2 text-white rounded transition-transform hover:scale-110">
+                    <a href="/amonestacionn" target="_blank">
+                        Desacargar
+                        Carta de amonestación
+                    </a>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                    </svg>
+                </button>
+                
             </div>
             <div class="flex mt-4 border-[#18A689]">
                 <button class="bg-[#18A689] p-2 text-white rounded transition-transform hover:scale-110" hidden>Subir
