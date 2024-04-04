a00cd8733f3ef9b84bd72b1397f13a65e87fa690
JuanCarlosRossLopez <berpyboy10@gmail.com>
2024-04-03 21:51:49 -0500

    Hola

--- sge_laravel/app/Models/presidencies.php
+++ sge_laravel/app/Models/presidencies.php
@@ -4,10 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class Presidencies extends Model
+class presidencies extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'president_name',
+        'president_lastname',
+        'payroll_president',
+        'user_id',
+        'division_id',
+        'career_id'
+    ];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
+    public function division():HasOne{
+        return $this->hasOne(Division::class, 'division_id');
+    }
+
+    public function career():HasOne{
+        return $this->hasOne(Career::class, 'career_id');
+    }
 }
