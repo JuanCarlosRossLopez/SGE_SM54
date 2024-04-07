025daebd5c607e56a8c1d8e38f057d46852d8391
cristian770h <cristianlopezrodriguez770@gmail.com>
2024-04-06 23:06:15 -0500

    lol

--- sge_laravel/resources/views/students/libros/addBookStudent.blade.php
+++ sge_laravel/resources/views/students/libros/addBookStudent.blade.php
@@ -1,4 +1,4 @@
-<div id="add"
+{{--<div id="add"
     class="modal2 h-screen w-full fixed left-0 top-0 hidden flex justify-center items-center bg-black bg-opacity-50">
     <div class="bg-[#01A080] w-max rounded shadow-lg max-w-4xl">
         <div class="border-b px-4 py-2 flex justify-between items-center text-center">
@@ -125,7 +125,9 @@
         </div>
     </div>
 </div>
-<script>
+--}}
+<script >
+    /*
     const count = 0;
 
     document.getElementById('addStudentButton').addEventListener('click', function() {
@@ -166,5 +168,5 @@
 
             count++;
         }
-    });
+    });*/
 </script>
