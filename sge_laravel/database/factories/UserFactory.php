0b790214777346d4bb57c5a3c3b9325c7a8d3322
JuanCarlosRossLopez <berpyboy10@gmail.com>
2024-04-10 02:05:59 -0500

    seeders de usuarios (uno por cada rol)

--- sge_laravel/database/factories/UserFactory.php
+++ sge_laravel/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -21,14 +22,83 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
     {
         return [
             'name' => 'Admin',
             'email' => 'admin@test.com',
             'email_verified_at' => now(),
-            'password' => 'admin1234'
+            'password' => Hash::make('admin1234'),
         ];
+    }
+
+    /**
+     * Define the state for a student user.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function student()
+    {
+        return $this->state([
+            'name' => 'Student',
+            'email' => 'student@test.com',
+            'password' => Hash::make('student1234'),
+        ]);
+    }
+
+    /**
+     * Define the state for a teacher user.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function teacher()
+    {
+        return $this->state([
+            'name' => 'Teacher',
+            'email' => 'teacher@test.com',
+            'password' => Hash::make('teacher1234'),
+        ]);
+    }
+
+    /**
+     * Define the state for a president user.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function president()
+    {
+        return $this->state([
+            'name' => 'President',
+            'email' => 'president@test.com',
+            'password' => Hash::make('president1234'),
+        ]);
+    }
+
+    /**
+     * Define the state for a coordinator user.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function coordinator()
+    {
+        return $this->state([
+            'name' => 'Coordinator',
+            'email' => 'coordinator@test.com',
+            'password' => Hash::make('coordinator1234'),
+        ]);
     }
 
     /**
