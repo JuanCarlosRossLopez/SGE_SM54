0059ec3e1434a507e5987c09f86f45a519ed07df
Marco-rc72 <marcocauichantonio@gmail.com>
2024-04-14 10:08:40 -0500

    Agregar grupos masivamente

--- sge_laravel/app/Models/Group.php
+++ sge_laravel/app/Models/Group.php
@@ -4,18 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Career;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Group extends Model
 {
     protected $fillable = [
+        'career_id',
         'group_name',
-        'career_id'
+        
     ];
     use HasFactory;
 
 
-    public function career()
+    public function career(): BelongsTo
     {
         return $this->belongsTo(Career::class, 'career_id');
+    }
+    public function division(): HasMany
+    {
+        return $this->hasMany(Group::class);
     }
 }
