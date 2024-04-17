43b5778c5c93bb25a6288438a5569b5a3bf9dd2b
Jonathan0890 <jonathan.coox15@gmail.com>
2024-04-17 08:55:05 -0500

    Error500

---
+++ sge_laravel/resources/views/errors/500.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>500 Error interno del servidor</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-100 h-screen flex flex-col justify-center items-center">
+    <div class="max-w-lg text-center">
+        <h1 class="text-4xl font-bold text-gray-800 mb-4">500 - Error interno del servidor</h1>
+        <p class="text-lg text-gray-600 mb-8">Lo sentimos, se ha producido un error interno en el servidor.</p>
+        <a href="#" onclick="history.go(-1)"
+            class="inline-block bg-green-500/20 text-green-900 font-semibold px-6 py-3 rounded-lg shadow-md hover:bg-green-600 transition-colors duration-300">Volver</a>
+    </div>
+</body>
+
+</html>
+
